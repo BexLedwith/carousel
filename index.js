c6bf40e16abc17baecff374f8f915f9ad45a41f0
BexLedwith <bex.akeakamai@gmail.com>
2021-06-29 17:55:31 -0400

    refactor

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const slides = document.getElementsByClassName("carousel-item");
 let slidePosition = 0;
 const totalSlides = slides.length;
 
-// document
-//   .getElementById("carousel-button-prev")
-//   .addEventListener("click", moveToPrevSlide);
+document
+  .getElementById("carousel-button-prev")
+  .addEventListener("click", moveToPrevSlide);
 
 document
   .getElementById("carousel-button-next")
@@ -19,20 +19,14 @@ function hideAllSlides() {
 
 function moveToNextSlide() {
   hideAllSlides();
-  if (slidePosition === totalSlides - 1) {
-    slidePosition = 0;
-  } else {
-    slidePosition++;
-  }
-
-  //   slidePosition === totalSlides - 1 ? (slidePosition = 0) : slidePosition++;
+  slidePosition === totalSlides - 1 ? (slidePosition = 0) : slidePosition++;
   slides[slidePosition].classList.add(
     "carousel-item-visible"
   ); /*make current slide visible*/
 }
 
-// function moveToPrevSlide() {
-//   hideAllSlides();
-//   slidePosition === 0 ? (slidePosition = totalSlides - 1) : slidePosition--;
-//   slides[slidePosition].classList.add("carousel-item-visible");
-// }
+function moveToPrevSlide() {
+  hideAllSlides();
+  slidePosition === 0 ? (slidePosition = totalSlides - 1) : slidePosition--;
+  slides[slidePosition].classList.add("carousel-item-visible");
+}
